cb26f9399d3a92cbfa615f8b1861ba433baa922f
MarkChang <h03514@gmail.com>
2021-04-30 19:16:57 +0800

    1.新增methods 做串接API準備 2.修改HTML樣式

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 const vm = Vue.createApp({
     data() {
         return {
-            disabled: 0
+            disabled: 1
+        }
+    },
+    methods: {
+        search() {
+            let selVal = document.querySelector('#insOrQueryFunc').value;
+            let obj = {};
+            obj.userName = document.querySelector('#userName').value;
+            obj.enName = document.querySelector('#enName').value;
+            obj.location = document.querySelector('#location').value;
+            obj.companyCode = document.querySelector('#companyCode').value;
+            obj.sn = document.querySelector('#sn').value;
+            obj.device = document.querySelector('#device').value;
+            obj.type = document.querySelector('#type').value;
+            obj.buyDate = document.querySelector('#buyDate').value;
+            obj.remark = document.querySelector('#remark').value;
+            cslog(obj)
+            if (selVal == 'Query') {
+
+            }
+
+
         }
     }
 });
